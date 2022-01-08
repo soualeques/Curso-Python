a54a15dd0a220496ba429f3c1de85738df2468c6
ALEX NASCIMENTO DO CARMO <82433728+soualeques@users.noreply.github.com>
2022-01-08 02:02:29 -0300

    aula 08/01

---
+++ exercicios/Mundo2/Conversor_de_Bases.py
@@ -0,0 +1,19 @@
+'''Escreva um programa que leia um numero inteiro qualquer e peça 
+para o usuario escolher qual sera a base de conversao:
+1- para BINARIO
+2- para OCTAL
+3- para HEXADECIMAL'''
+
+numero = int(input("Digite um numero inteiro qualquer: "))
+base = int(input('''Escolha uma das bases para conversao:
+                 [1] para BINARIO
+                 [2] para OCTAL
+                 [3] para HEXADECIMAL
+                 sua opção: '''))
+
+if base == 1:
+    print("o numero {} em BINARIO fica \033[4;31m{}\033[m".format(numero, bin(numero)))
+elif base == 2:   
+    print(f"o numero{numero} em OCTAL fica \033[4;31m{oct(numero)}\033[m")
+elif base == 3:
+    print(f"o numero {numero} em HEXADECIMAL fica \033[4:31m{hex(numero)}\033[m")
