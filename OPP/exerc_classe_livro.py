559fc211b9057494d8b7724733fe3b5868e1592e
ALEX NASCIMENTO DO CARMO <82433728+soualeques@users.noreply.github.com>
2022-04-23 16:37:53 -0300

    UPDATE

---
+++ OPP/exerc_classe_livro.py
@@ -0,0 +1,12 @@
+class Livro():
+    def __init__(self):
+        self.livro = 'Sherlock Holmes'
+        self.genero = 'Suspense'
+        self.autor = 'Arthur Conan Doyle'
+        self.numero_paginas = 500
+        
+livro = Livro()
+print(livro.livro)
+print(livro.genero)
+print(livro.autor)
+print(livro.numero_paginas)
